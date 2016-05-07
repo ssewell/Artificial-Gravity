6a824ddf0c56c348e33a2bd4cb874d3d084ad79f
Daniel Quinlivan <darknrgy@gmail.com>
2016-05-07 11:22:59 -0700

    Initial commit for artificial gravity project

---
+++ Assets/src/ReverseNormals.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class ReverseNormals : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        // ReverseFaces();
+        // AssetDatabase.CreateAsset(mesh, "Assets/AsteroidNormalizedFaces.asset");
+    }
+
+    void ReverseFaces() {
+        MeshFilter filter = GetComponent(typeof(MeshFilter)) as MeshFilter;
+        if (filter != null) {
+            Mesh mesh = filter.mesh;
+
+            Vector3[] normals = mesh.normals;
+            for (int i = 0; i < normals.Length; i++)
+                normals[i] = -normals[i];
+            mesh.normals = normals;
+
+            for (int m = 0; m < mesh.subMeshCount; m++) {
+                int[] triangles = mesh.GetTriangles(m);
+                for (int i = 0; i < triangles.Length; i += 3) {
+                    int temp = triangles[i + 0];
+                    triangles[i + 0] = triangles[i + 1];
+                    triangles[i + 1] = temp;
+                }
+                mesh.SetTriangles(triangles, m);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/src/RotatingAxisFriction.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class RotatingAxisFriction : MonoBehaviour {
+
+    
+    public Vector3 axis;
+    public GameObject target;
+    public bool clockwise = true;
+    public Camera camera;
+
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ApplyForce(target);
+    }
+
+    public void ApplyForce(GameObject target) {
+
+        // Setup variables
+        float angularVelocity = GetComponent<Rigidbody>().angularVelocity.magnitude;
+        Vector3 targetVector = target.transform.position;
+        Vector3 targetVelocityVector = target.GetComponent<Rigidbody>().velocity;
+        ConstantForce targetForce = target.GetComponent<ConstantForce>();
+
+        // Get air vector
+        Vector3 forceVectorNormalized = GetForceVectorNormalized(targetVector);
+        float radius = GetDeltaVector(GetClosestPointOnAxis(targetVector), targetVector).magnitude;
+        float forceMagnitude = GetLinearVelocity(angularVelocity, radius);
+        Vector3 forceVector = forceVectorNormalized * forceMagnitude;
+        Vector3 frictionVector = forceVector * Time.deltaTime;
+
+        // Apply air vector to target
+        Vector3 frictionVelocityVector = forceVector - targetVelocityVector;
+        targetForce.force = frictionVelocityVector.normalized * (float)Math.Pow(frictionVelocityVector.magnitude, 2);
+
+        //target.GetComponent<Rigidbody>().transform.LookAt(targetVector - targetVelocityVector);
+        // target.GetComponent<Rigidbody>().rotation = GetComponent<Rigidbody>().rotation;
+        // target.transform.LookAt(targetVector + targetVelocityVector);
+        // target.transform.LookAt(targetVector + targetVelocityVector);
+        target.transform.LookAt(target.transform.position - frictionVelocityVector);
+    }
+
+    // Convert angular velocity and radians to linear velocity
+    protected float GetLinearVelocity(float angularVelocity, float radius) {
+        return angularVelocity * radius;
+    }
+
+
+    // Calcululate the closest point to the target that is on the axis line
+    protected Vector3 GetClosestPointOnAxis(Vector3 target) {
+        return new Vector3(0, target.y, 0);
+    }
+
+    // Get the difference between two vectors
+    protected Vector3 GetDeltaVector(Vector3 a, Vector3 b) {
+        return b - a;
+    }
+
+    // Get the vector of the moving air
+    protected Vector3 GetForceVectorNormalized(Vector3 target) {
+        Vector3 axisHelper = GetClosestPointOnAxis(target);
+        Vector3 deltaVector = GetDeltaVector(axisHelper, target); // target - axisHelper;
+        float forceMagnitude = deltaVector.magnitude;
+        Vector3 forceVector = Vector3.Cross(target, axisHelper).normalized;
+        forceVector = clockwise ? -forceVector : forceVector;
+
+        return forceVector;
+    }
+}
