62d7e93e38f7d1b908ebc1cd0dd6e9f0e510258e
David <david@Davids-Air.attlocal.net>
2016-05-11 22:48:49 -0700

    First Commit for Magnetic Shoes concept. I cast a vector down, and if it intersects, apply a force towards down, relative to the object. We can parameterize how far away the ground is before the shoes take effect, and the starting strength of the shoes. The player can control the strength of the shoes by , and .

---
+++ Assets/MagneticShoes.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagneticShoes : MonoBehaviour {
+
+	public float InitalStrength;
+	public float DistanceBeforeShoesTakeEffect;
+	public bool DebugDraw;
+
+
+	private float CurrentStrength;
+	// Use this for initialization
+	void Start () {
+		CurrentStrength = InitalStrength;
+	}
+
+	void FixedUpdate () {
+		if (Input.GetKeyUp (KeyCode.Comma)) {
+			CurrentStrength += 1.0f;
+		} else if (Input.GetKeyUp (KeyCode.Period)) {
+			CurrentStrength -= 1.0f;
+		}
+			
+		Vector3 dir = -gameObject.transform.up;
+		RaycastHit hit = new RaycastHit ();
+		if (DebugDraw) {
+			Debug.DrawRay (transform.position, dir * DistanceBeforeShoesTakeEffect, Color.green);
+		}
+			
+		if (Physics.Raycast (transform.position, dir, out hit, DistanceBeforeShoesTakeEffect)) {
+			float inverseDistanceSquared = 1.0f / Mathf.Min(hit.distance * hit.distance, 0.00001f); // Avoid division by 0
+			gameObject.GetComponent<Rigidbody> ().AddRelativeForce (dir * CurrentStrength * inverseDistanceSquared);
+		}
+	}
+}
