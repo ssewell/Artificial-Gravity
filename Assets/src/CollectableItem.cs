7f361cada7c84f11d8a9bd036ea4546be461b636
Shane Sewell <ssewell@gmail.com>
2016-05-15 15:21:02 -0500

    Added collectable items
    Added the ability to collect items, which can optionally add behaviors
    to the player object.

---
+++ Assets/src/CollectableItem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectableItem : MonoBehaviour {
+
+    /// <summary>
+    /// When the collectable item collides with the player controller, the player will gain this behavior
+    /// </summary>
+    public string BehaviorName;
+
+    /// <summary>
+    /// An optional sound that is played when the user collects the item
+    /// </summary>
+    public AudioClip CollectSound;
+	
+	/// <summary>
+    /// Collectable items rotate, so it's more apparent to the user that it's colletable
+    /// </summary>
+	void Update () {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
